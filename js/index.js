820018b24111fac5d91a875fb43fe6e280ffc2b3
Esteban <bestebanpadilla@gmail.com>
2023-02-03 21:56:05 -0300

    Funcionalidad para mostrar la alerta de ingreso de texto para cifrar implementada

--- js/index.js
+++ js/index.js
@@ -9,6 +9,20 @@ const showTextOnResponseScreen = (text) => {
     btnCopy.style.display = 'block';
 };
 
+const noneText = () => {
+    responseP.textContent = '';
+    console.log('Crack' + responseP.textContent);   
+    responseScreenResponse.style.justifyContent = 'center';
+    muñeco.style.display = 'block';
+    responseH2.style.display = 'block';
+    btnCopy.style.display = 'none';
+    responseP.textContent = 'Ingresa el texto que desees encriptar o desencriptar';
+    responseP.style.textAlign = 'center';
+    responseP.style.fontSize = '1.6rem';
+    responseP.style.overflowY = 'hidden';
+    responseP.style.display = 'block';
+};
+
 const copyToClipboard = () => {
     let responseText = responseP.textContent;
     navigator.clipboard.writeText(responseText);
@@ -16,42 +30,60 @@ const copyToClipboard = () => {
 
 const decrypt = () => {
     let textAreaContent = textArea.value
-    textAreaContent = textAreaContent.replaceAll(/ai/g, 'a');
-    textAreaContent = textAreaContent.replaceAll(/enter/g, 'e');
-    textAreaContent = textAreaContent.replaceAll(/imes/g, 'i');
-    textAreaContent = textAreaContent.replaceAll(/ober/g, 'o');
-    textAreaContent = textAreaContent.replaceAll(/ufat/g, 'u');
-    showTextOnResponseScreen(textAreaContent);
+    if(textAreaContent === ""){
+        if(muñeco.style.display === 'none'){
+            noneText();
+        }
+        else {
+            return false;
+        }
+    }else{
+        textAreaContent = textAreaContent.replaceAll(/ai/g, 'a');
+        textAreaContent = textAreaContent.replaceAll(/enter/g, 'e');
+        textAreaContent = textAreaContent.replaceAll(/imes/g, 'i');
+        textAreaContent = textAreaContent.replaceAll(/ober/g, 'o');
+        textAreaContent = textAreaContent.replaceAll(/ufat/g, 'u');
+        showTextOnResponseScreen(textAreaContent);
+    };
 };
 
 const encrypt = () => {
     let textAreaContent = textArea.value
-    textAreaContent = textAreaContent.toLowerCase();
-    textAreaContent = textAreaContent.split('');
-    textAreaContent = textAreaContent.map((char) => {
-        switch (char) {
-            case 'a': 
-                char = char.replace('a', 'ai');
-                break;
-            case 'e':
-                char = char.replace('e', 'enter');
-                break;
-            case 'i':
-                char = char.replace('i', 'imes');
-                break;
-            case 'o':
-                char = char.replace('o', 'ober');
-                break;
-            case 'u':
-                char =char.replace('u', 'ufat');
-                break;
-            default:
-                break;
+    if(textAreaContent === ""){
+        if(muñeco.style.display === 'none'){
+            noneText();
         }
-        return char;
-    })
-    textAreaContent = textAreaContent.join('');
-    showTextOnResponseScreen(textAreaContent);
+        else {
+            return false;
+        }
+    } else {
+        textAreaContent = textAreaContent.toLowerCase();
+        textAreaContent = textAreaContent.split('');
+        textAreaContent = textAreaContent.map((char) => {
+            switch (char) {
+                case 'a': 
+                    char = char.replace('a', 'ai');
+                    break;
+                case 'e':
+                    char = char.replace('e', 'enter');
+                    break;
+                case 'i':
+                    char = char.replace('i', 'imes');
+                    break;
+                case 'o':
+                    char = char.replace('o', 'ober');
+                    break;
+                case 'u':
+                    char =char.replace('u', 'ufat');
+                    break;
+                default:
+                    break;
+            }
+            return char;
+        })
+        textAreaContent = textAreaContent.join('');
+        showTextOnResponseScreen(textAreaContent);
+    };
 };
 
 const textArea = document.querySelector('.encrypter__text');
